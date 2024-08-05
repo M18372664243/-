4df0edac27482678fa7f7fc39a7b9882243971b4
mfw <18372664243@163.com>
2024-08-05 20:04:01 +0800

    Revert "Changes"
    This reverts commit ef4fe5f52308e9125afe0f87fe6f4e9f265424ac.

--- main/autoStack.lua
+++
@@ -1,9 +0,0 @@
-local slotmachineutils = require "modules.slotmachineutils"
-AddComponentPostInit("lootdropper", function(LootDropper, inst)
-    local old_FlingItem = LootDropper.FlingItem
-    function LootDropper:FlingItem(loot, pt)
-        old_FlingItem(LootDropper, loot, pt)
-        local pos = inst:GetPosition()
-        loot:DoTaskInTime(0.5, function() slotmachineutils.auto_stack_fn(pos, loot) end)
-    end
-end)
--- main/chooseLanguage.lua
+++
@@ -1,6 +0,0 @@
---语言
-if TUNING.SLOTMACHINE_MODCONFIGDATA["Language"] =="ch" then
-    modimport("languages/chinese_s")
-else
-    modimport("languages/english")
-end
